da8cf7a5e356dd3ae3a6d631cbbdedf61d274c08
cnnamani <nnamanichisomnc@gmail.com>
2026-03-26 17:48:29 +0100

    renamed art_jsutify.go to ascii_art.go and added comments

--- art_justify/ascii_art.go
+++ art_justify/ascii_art.go
@@ -15,13 +15,16 @@ func AsciiArt(text, banner string) [][]string {
 		log.Fatal("Error: ", err)
 	}
 
-	// Split banner into lines
+	// Split banner into lines - f
+	// Becomes slice of strings
 	arts := strings.Split(string(content), "\n")
 
 	// Convert escaped "\n" into real newline
 	text = strings.ReplaceAll(text, "\\n", "\n")
 
-	// Split input into lines
+	// Split input into lines 
+	// Becomes a slice of strings
+	// This helps in vertical layout of the split input words
 	lines := strings.Split(text, "\n")
 
 	var result [][]string
@@ -30,32 +33,34 @@ func AsciiArt(text, banner string) [][]string {
 
 		// Handle empty line (only ONE blank line, not 8 rows)
 		if line == "" {
-			empty := make([]string, 8) // create 8 empty row
+			empty := make([]string, 8) // create 8 empty row that serves as vertical gaps between words
 			result = append(result, empty)
 			continue
 		}
 
 		// split line into words
+		// Helps in horizontal layout and removes extra spacing
+		// Because we will control the spaces manually
 		words := strings.Fields(line)
 
 		for _, word := range words {
 			rows := make([]string, 8)
 
 			for row := 0; row < 8; row++ {
-				var strBuild strings.Builder
+				var strBuild strings.Builder // To avoid repeated memory allocation due to repeated string concatenation
 
 				for _, ch := range word {
 
 					if ch < 32 || ch > 126 {
 						continue
 					}
-					index := (int(ch) - 32) * 9
-					strBuild.WriteString(arts[index+row+1])
+					index := (int(ch) - 32) * 9 // Gets position of the seperator line for each character
+					strBuild.WriteString(arts[index+row+1]) // Skips the seperator line here and moves down the character
 				}
 
-				rows[row] = strBuild.String()
+				rows[row] = strBuild.String() // Save each word, after building the full row for it
 			}
-			result = append(result, rows)
+			result = append(result, rows) // Each word becomes a slice of string
 		}
 	}
 
