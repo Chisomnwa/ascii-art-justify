9f94090eaad2ded427b81e5062a7fbd5fef48f6d
cnnamani <nnamanichisomnc@gmail.com>
2026-05-07 10:19:03 +0100

    Correct the align.go and main.go logic

--- main.go
+++ main.go
@@ -3,86 +3,77 @@ package main
 import (
 	"ascii-justify/art_justify"
 	"fmt"
+	"log"
 	"os"
+	"strings"
 )
 
 func main() {
-	text := os.Args[1]
-	banner := os.Args[2] + ".txt"
-	width:= art_justify.GetTerminalWidth()
-	art_text := art_justify.AsciiArt(text, banner)
+	if len(os.Args) < 2 || len(os.Args) > 4 {
+		log.Fatal("\nUsage: go run . [OPTION] [STRING] [BANNER]\n\nExample: go run . --align=right something standard")
+	}
 
-	// art_justify.LeftAlign(art_text)
-	//art_justify.RightAlign(art_text, width)
-	//art_justify.CenterAlign(art_text, width)
-	art_justify.JustifyAlign(art_text, text, width, banner)
-	fmt.Println(width)
+	text := ""
+	banner := "standard.txt"
+	alignment := "left"
 
-	// if len(os.Args) < 2 || len(os.Args) > 4 {
-	// 	log.Fatal("\nUsage: go run . [OPTION] [STRING] [BANNER]\n\nExample: go run . --align=right something standard")
-	// }
+	if len(os.Args) == 2 {
+		text = os.Args[1]
 
-	// text := ""
-	// banner := "standard.txt"
-	// alignment := "left"
+		if text == "" {
+			return
+		}
 
-	// if len(os.Args) == 2 {
-	// 	text = os.Args[1]
+		if text == "\\n" {
+			fmt.Println()
+			return
+		}
 
-	// 	if text == "" {
-	// 		return
-	// 	}
+	} else if len(os.Args) == 3 {
+		arg := os.Args[1]
 
-	// 	if text == "\\n" {
-	// 		fmt.Println()
-	// 		return
-	// 	}
+		if strings.HasPrefix(arg, "--align=") {
+			alignment = strings.TrimPrefix(arg, "--align=")
+			text = os.Args[2]
+		} else {
+			text = os.Args[1]
+			banner = os.Args[2] + ".txt"
+		}
 
-	// } else if len(os.Args) == 3 {
-	// 	arg := os.Args[1]
+	} else if len(os.Args) == 4 {
+		arg := os.Args[1]
 
-	// 	if strings.HasPrefix(arg, "--align=") {
-	// 		alignment = strings.TrimPrefix(arg, "--align=")
-	// 		text = os.Args[2]
-	// 	} else {
-	// 		text = os.Args[1]
-	// 		banner = os.Args[2] + ".txt"
-	// 	}
+		if !strings.HasPrefix(arg, "--align=") {
+			log.Fatal("Invalid align flag. Expected format: --align=left|right|center|justify")
+		}
 
-	// } else if len(os.Args) == 4 {
-	// 	arg := os.Args[1]
+		alignment = strings.ToLower(strings.TrimPrefix(arg, "--align="))
+		text = os.Args[2]
+		banner = os.Args[3] + ".txt"
+	}
 
-	// 	if !strings.HasPrefix(arg, "--align=") {
-	// 		log.Fatal("Invalid align flag. Expected format: --align=left|right|center|justify")
-	// 	}
+	// get terminal width for alignment calculations
+	total_width := art_justify.GetTerminalWidth()
+	if len(text) > total_width {
+		log.Fatal("Terminal size exceeded")
+	}
 
-	// 	alignment = strings.ToLower(strings.TrimPrefix(arg, "--align="))
-	// 	text = os.Args[2]
-	// 	banner = os.Args[3] + ".txt"
-	// }
+	// Generate ASCII
+	result := art_justify.AsciiArt(text, banner)
 
-	// // get terminal width for alignment calculations
-	// total_width := art_justify.GetTerminalWidth()
-	// if len(text) > total_width {
-	// 	log.Fatal("Terminal size exceeded")
-	// }
+	// Applying the alignment
+	switch alignment {
+	case "left":
+		art_justify.LeftAlign(result)
+	case "right":
+		art_justify.RightAlign(result, total_width)
+	case "center":
+		art_justify.CenterAlign(result, total_width)
+	case "justify" :
+		art_justify.JustifyAlign(result, total_width)
 
-	// // Generate ASCII
-	// result := art_justify.AsciiArt(text, banner)
-
-	// // Applying the alignment
-	// switch alignment {
-	// case "left":
-	// 	art_justify.LeftAlign(result)
-	// case "right":
-	// 	art_justify.RightAlign(result, total_width)
-	// case "center":
-	// 	art_justify.CenterAlign(result, total_width)
-	// case "justify" :
-	// 	art_justify.JustifyAlign(result, total_width)
-
-	// default:
-	// 	(log.Fatal("error"))
-	// }
+	default:
+		(log.Fatal("error"))
+	}
 
 }
